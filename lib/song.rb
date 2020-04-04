69899f680914581c2f8c23996b6cf51fff6856a5
matthew enbar <matthew.enbar@gmail.com>
2020-04-04 18:34:37 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,23 @@
+class Song
+
+  attr_accessor :name, :artist, :genre
+
+  @@all = []
+
+  def initialize (name)
+    @name = name
+    save
+  end
+
+  def save
+      @@all << self
+    end
+
+    def self.all
+      @@all
+    end
+
+
+
+
+end
